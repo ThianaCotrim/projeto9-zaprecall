df0eb5c3156221d0e4f71681044d4ac34d2d7d43
Thiana <thianacantunes@gmail.com>
2023-02-28 17:00:30 -0300

    Componente Flashcards criado, e inserir o overflow no css

--- src/App.js
+++ src/App.js
@@ -1,54 +1,21 @@
 import image0 from "./images/image2.png"
-import vector from "./images/vector.png"
+import Flashcards from "./components/Flashcards";
 
-
-function App() {
+const App = () => {
   return (
     <div className="container">
       <div className="titulo">
         <img src={image0} alt={image0}/>
       ZapRecall
       </div>
-      < div className="box-maior">
-       <div className="flashcard">
-        <div className="pergunta">
-        Pergunta 1
-       </div>
-        <div className="vector">
-        <img src={vector} alt={vector}/> 
-        </div>
-        </div> 
-      <div className="flashcard">
-      <div className="pergunta">
-        Pergunta 2
-       </div>
-        <div className="vector">
-        <img src={vector} alt={vector}/> 
-        </div>
-      </div> 
-      <div className="flashcard">
-      <div className="pergunta">
-        Pergunta 3
-       </div>
-        <div className="vector">
-        <img src={vector} alt={vector}/> 
-        </div>
-      </div> 
-      <div className="flashcard">
-      <div className="pergunta">
-        Pergunta 4
-       </div>
-        <div className="vector">
-        <img src={vector} alt={vector}/> 
-        </div>
-      </div> 
-      </div>
+
+      < Flashcards />
+     
       <div className="box-low">
         0/4 CONCLUÍDOS 
       </div>
     </div>
   );
 }
-
 
 export default App
---
+++ src/components/Flashcards.js
@@ -0,0 +1,96 @@
+import vector from "../images/vector.png"
+
+
+const Flashcards = () => {
+    return (
+  
+      < div className="flascard-container">
+      <div className="flashcard">
+      <div className="pergunta">
+      Pergunta 1
+     </div>
+      <button>
+      <img src={vector} alt={vector}/> 
+      </button>
+      </div> 
+  
+  
+  
+    <div className="flashcard">
+    <div className="pergunta">
+      Pergunta 2
+     </div>
+     <button>
+      <img src={vector} alt={vector}/> 
+      </button>
+    </div> 
+  
+  
+  
+    <div className="flashcard">
+    <div className="pergunta">
+      Pergunta 3
+     </div>
+     <button>
+      <img src={vector} alt={vector}/> 
+      </button>
+    </div> 
+  
+  
+  
+    <div className="flashcard">
+    <div className="pergunta">
+      Pergunta 4
+     </div>
+     <button>
+      <img src={vector} alt={vector}/> 
+      </button>
+    </div> 
+  
+  
+  
+    <div className="flashcard">
+    <div className="pergunta">
+      Pergunta 5
+     </div>
+     <button>
+      <img src={vector} alt={vector}/> 
+      </button>
+    </div> 
+  
+  
+    <div className="flashcard">
+    <div className="pergunta">
+      Pergunta 6
+     </div>
+     <button>
+      <img src={vector} alt={vector}/> 
+      </button>
+    </div> 
+  
+  
+  
+    <div className="flashcard">
+    <div className="pergunta">
+      Pergunta 7
+     </div>
+     <button>
+      <img src={vector} alt={vector}/> 
+      </button>
+    </div> 
+  
+  
+    <div className="flashcard">
+    <div className="pergunta">
+      Pergunta 8
+     </div>
+     <button>
+      <img src={vector} alt={vector}/> 
+      </button>
+    </div> 
+    </div>
+  
+    )
+  }
+
+  export default Flashcards 
