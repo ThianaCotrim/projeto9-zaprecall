37d615e8f422491b3e678353600b45d75d7e61b2
Thiana <thianacantunes@gmail.com>
2023-02-28 21:37:22 -0300

    versão com Styled Components

--- src/App.js
+++ src/App.js
@@ -1,21 +1,68 @@
 import image0 from "./images/image2.png"
 import Flashcards from "./components/Flashcards";
+import styled from "styled-components";
 
 const App = () => {
   return (
-    <div className="container">
-      <div className="titulo">
+    <Container>
+      <Titulo>
         <img src={image0} alt={image0}/>
       ZapRecall
-      </div>
-
+      </Titulo>
       < Flashcards />
      
-      <div className="box-low">
+      <BoxLow>
         0/4 CONCLUÍDOS 
-      </div>
-    </div>
+      </BoxLow>
+    </Container>
   );
 }
 
 export default App
+
+const Container = styled.div `
+    background-color: #FB6B6B;
+    width: 375px;
+    height: 667px;
+    border: 1px solid #DBDBDB;
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    align-items: center;
+    margin-top: 50px;
+`;
+
+const Titulo = styled.div `
+    color: #FFFFFF;
+    width: 230px;
+    height: 44px;
+    margin-top: 48px;
+    font-family: 'Righteous', cursive;
+    font-size: 36px;
+    font-weight: 400;
+    display: flex;
+    text-align: center;
+    justify-content: space-between;
+    align-items: center;
+    img {
+    width: 52px;
+    height: 60px;
+    }
+`;
+
+const BoxLow = styled.div `
+    background-color: #FFFFFF;
+    width: 376px;
+    height: 70px;
+  
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-family: 'Recursive', sans-serif;
+    font-size: 18px;
+    font-weight: 400;
+    color: #333333;
+    box-shadow: 0px -4px 6px rgba(0, 0, 0, 0.05);
+    position: fixed;
+    margin-top: 597px;
+`;
--- src/components/Flashcards.js
+++ src/components/Flashcards.js
@@ -1,96 +1,132 @@
 import vector from "../images/vector.png"
+import styled from "styled-components"
 
 
 const Flashcards = () => {
     return (
   
-      < div className="flascard-container">
-      <div className="flashcard">
-      <div className="pergunta">
+      <FlascardContainer>
+      <Flashcard>
+      <Pergunta>
       Pergunta 1
-     </div>
-      <button>
+     </Pergunta>
+      <Button>
       <img src={vector} alt={vector}/> 
-      </button>
-      </div> 
+      </Button>
+      </Flashcard> 
   
   
   
-    <div className="flashcard">
-    <div className="pergunta">
+    <Flashcard>
+    <Pergunta>
       Pergunta 2
-     </div>
-     <button>
+     </Pergunta>
+     <Button>
       <img src={vector} alt={vector}/> 
-      </button>
-    </div> 
+      </Button>
+    </Flashcard> 
   
   
   
-    <div className="flashcard">
-    <div className="pergunta">
+    <Flashcard>
+    <Pergunta>
       Pergunta 3
-     </div>
-     <button>
+     </Pergunta>
+     <Button>
       <img src={vector} alt={vector}/> 
-      </button>
-    </div> 
+      </Button>
+    </Flashcard> 
   
   
   
-    <div className="flashcard">
-    <div className="pergunta">
+    <Flashcard>
+    <Pergunta>
       Pergunta 4
-     </div>
-     <button>
+     </Pergunta>
+     <Button>
       <img src={vector} alt={vector}/> 
-      </button>
-    </div> 
+      </Button>
+    </Flashcard> 
   
   
   
-    <div className="flashcard">
-    <div className="pergunta">
+    <Flashcard>
+    <Pergunta>
       Pergunta 5
-     </div>
-     <button>
+     </Pergunta>
+     <Button>
       <img src={vector} alt={vector}/> 
-      </button>
-    </div> 
+      </Button>
+    </Flashcard> 
   
   
-    <div className="flashcard">
-    <div className="pergunta">
+    <Flashcard>
+    <Pergunta>
       Pergunta 6
-     </div>
-     <button>
+     </Pergunta>
+     <Button>
       <img src={vector} alt={vector}/> 
-      </button>
-    </div> 
+      </Button>
+    </Flashcard> 
   
   
   
-    <div className="flashcard">
-    <div className="pergunta">
+    <Flashcard>
+    <Pergunta>
       Pergunta 7
-     </div>
-     <button>
+     </Pergunta>
+     <Button>
       <img src={vector} alt={vector}/> 
-      </button>
-    </div> 
+      </Button>
+    </Flashcard> 
   
   
-    <div className="flashcard">
-    <div className="pergunta">
+    <Flashcard>
+    <Pergunta>
       Pergunta 8
-     </div>
-     <button>
+     </Pergunta>
+     <Button>
       <img src={vector} alt={vector}/> 
-      </button>
-    </div> 
-    </div>
+      </Button>
+    </Flashcard> 
+    </FlascardContainer>
   
     )
   }
 
   export default Flashcards 
+
+  const FlascardContainer = styled.div`
+    width: 300px;
+    height: 100%;
+    margin-top: 50px;
+    overflow: hidden;
+    overflow-y: visible;
+    margin-bottom: 60px;
+  `;
+
+  const Flashcard = styled.div`
+    width: 300px;
+    height: 65px;
+    background-color: #FFFFFF;
+    border-radius: 5px;
+    box-shadow: 0px 4px 5px rgba(0, 0, 0, 0.15);
+    margin-bottom: 25px;
+    font-family: 'Recursive', sans-serif;
+    font-size: 16px;
+    font-weight: 700;
+    color: #333333;
+
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  `;
+
+  const Pergunta = styled.div `
+    margin-left: 15px;
+  `;
+
+  const Button = styled.button`
+    all: unset;
+    margin-right: 15px;
+  `
